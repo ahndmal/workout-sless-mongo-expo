ff337f660af1a3e36f6f0e112e4f16bdf3673fa4
malandr <quadr988@gmail.com>
2022-10-01 14:17:11 +0300

    init

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+} from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [workouts, setWorkouts] = useState([]);
+  const [workLoaded, setWorkLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch(
+      "https://us-central1-workouts-app-363616.cloudfunctions.net/go_gcp_cfunc_mongo_workouts"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setWorkouts(res);
+        setWorkLoaded(true);
+      });
+  }, []);
+
+  const Workout = (workout) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{workout.day}</Text>
     </View>
+  );
+
+  const renderItem = ({ item }) => <Workout title={item.day} />;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text>Workouts</Text>
+        <FlatList
+          data={workouts}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.record}
+        />
+        {/* {workLoaded &&
+          workouts.map((w) => (
+            <Text key={w.record}>
+              <Text>{w.comments}</Text>
+              <Text>{w.sets}</Text>
+            </Text>
+          ))} */}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
