0a7eb56097d4a75e73ce9cdded37b2a89800541a
malandr <quadr988@gmail.com>
2022-10-04 20:19:51 +0300

    pages

--- component/pages/WorkoutPage.jsx
+++ component/pages/WorkoutPage.jsx
@@ -3,23 +3,61 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import { getWorkoutById } from "../../api/WorkoutService";
 
 const WorkoutPage = ({ route }) => {
-  const [workoutLoaded, setWorkoutLoaded] = useState(true);
+  const [workoutLoaded, setWorkoutLoaded] = useState(false);
   const [workout, setworkout] = useState(null);
-  //   useEffect(() => {
-  //     getWorkoutById(record).then((res) => {
-  //       setWorkoutLoaded(true);
-  //       setworkout(res);
-  //       console.log(res);
-  //     });
-  //   }, []);
+  const [workoutTitle, setWorkoutTitle] = useState("");
+  useEffect(() => {
+    setWorkoutLoaded(true);
+    setWorkoutTitle(route.params.workout.workout_type);
+    // getColorByType(route.params.workout.workout_type);
+  }, []);
 
-  console.log(route.params.workout.record);
+  function getColorByType(type) {
+    let workoutColor = "grey";
+    switch (type) {
+      case "TRICEPS":
+        workoutColor = "purple";
+        // setWorkoutTitle("TRICEPS");
+        break;
+      case "PECS":
+        workoutColor = "blue";
+        // setWorkoutTitle("PECS");
+        break;
+      case "BICEPS":
+        workoutColor = "green";
+        // setWorkoutTitle("BICEPS");
+        break;
+      case "DELTS":
+        workoutColor = "orange";
+        // setWorkoutTitle("DELTS");
+        break;
+      case "ABS":
+        workoutColor = "black";
+        // setWorkoutTitle("ABS");
+        break;
+      case "BACK":
+        workoutColor = "red";
+        // setWorkoutTitle("BACK");
+        break;
+      case "QUADS":
+        workoutColor = "yellow";
+        // setWorkoutTitle("QUADS");
+        break;
+      default:
+        break;
+    }
+    return workoutColor;
+  }
 
   return (
     <View style={styles.container}>
       {workoutLoaded && (
         <>
-          <Text style={styles.title}>Workout</Text>
+          <Button
+            style={{ margin: 3 }}
+            title={workoutTitle}
+            color={getColorByType(route.params.workout.workout_type)}
+          />
           <Text>{route.params.workout.day}</Text>
           <Text>{route.params.workout.workout_type}</Text>
         </>
