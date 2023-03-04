faf9940668938f8ac05eec6c7e204b5b316a8197
Andrii Maliuta <quadr988@gmail.com>
2023-03-04 16:54:56 +0200

    gcp new funcs

--- api/WorkoutService.js
+++ api/WorkoutService.js
@@ -1,41 +1,40 @@
+// Functions URLs
+const GET_ALL_WORKOUTS =
+  "https://us-central1-workouts-app2.cloudfunctions.net/go_gcp_cfunc_mongo_workouts";
+const GET_WORKOUT = "https://get-workout-go-zj35iavtfa-uc.a.run.app";
+const CREATE_WORKOUT =
+  "https://create-workout-mongo-python-zj35iavtfa-uc.a.run.app/"; // Python
+
+// APIs
 const getWorkoutById = async (record) => {
-  const url = `https://us-central1-workouts-app-363616.cloudfunctions.net/GetWorkout?record=${record}`;
+  const url = `${GET_WORKOUT}?record=${record}`;
   const resp = await fetch(url);
   return await resp.json();
 };
 
 const getWorkouts = async (wType, wDate, month, year) => {
   if (wType !== undefined) {
-    const resp = await fetch(
-      `https://us-central1-workouts-app-363616.cloudfunctions.net/go_gcp_cfunc_mongo_workouts?wType=${wType}`
-    );
+    const resp = await fetch(`${GET_ALL_WORKOUTS}?wType=${wType}`);
     return await resp.json();
   } else if (wDate !== undefined) {
-    const resp = await fetch(
-      `https://us-central1-workouts-app-363616.cloudfunctions.net/go_gcp_cfunc_mongo_workouts?wDate=${wDate}`
-    );
+    const resp = await fetch(`${GET_ALL_WORKOUTS}?wDate=${wDate}`);
     return await resp.json();
   }
-  const resp = await fetch(
-    `https://us-central1-workouts-app-363616.cloudfunctions.net/go_gcp_cfunc_mongo_workouts`
-  );
+  const resp = await fetch(GET_ALL_WORKOUTS);
   return await resp.json();
 };
 
 async function createWorkout(data) {
   let dataBody = JSON.stringify(data);
   console.log(dataBody);
-  const response = await fetch(
-    "https://create-workout-mongo-python-lysuchhuma-uc.a.run.app/",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-cache",
-      body: dataBody,
-    }
-  );
+  const response = await fetch(CREATE_WORKOUT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-cache",
+    body: dataBody,
+  });
   return response.text();
 }
 
